936c4ab40906cb3b7f69f81079174d3c2a271025
FayRd <fayyadhrasid06@gmail.com>
2026-08-03 15:59:13 +0800

    feat: Add generated files for Flutter integration across iOS, macOS, Linux, and Windows

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\truer\\fvm\\versions\\3.44.8" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\mobile-development\\receipt_logging_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\truer\\fvm\\versions\\3.44.8"
+  "PROJECT_DIR=C:\\mobile-development\\receipt_logging_app"
+  "FLUTTER_ROOT=C:\\Users\\truer\\fvm\\versions\\3.44.8"
+  "FLUTTER_EPHEMERAL_DIR=C:\\mobile-development\\receipt_logging_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\mobile-development\\receipt_logging_app"
+  "FLUTTER_TARGET=C:\\mobile-development\\receipt_logging_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuOA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MDU4ZTBhZjJjMg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MGNkNjEwNzE3Yg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\mobile-development\\receipt_logging_app\\.dart_tool\\package_config.json"
+)
